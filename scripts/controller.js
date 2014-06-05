21a099d0bd11992eba8a580d61012ae26ed121c4
Ike Gandhi <ikegandhi@gmail.com>
2014-06-05 14:58:11 -0400

    initial commit

---
+++ scripts/controller.js
@@ -0,0 +1,7 @@
+var unspoil = function() {
+  var searchTerm = $('#searchTxt').val();
+  alert(searchTerm)
+}
+
+$('#content-main-heading').append("Keyword: <input type='text' id='searchTxt' name='term'></input> <br>");
+$('#content-main-heading').append("<input type='button' value='Unspoil' onclick='unspoil();'></input>");
