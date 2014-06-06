00cce614c3294748c1d042d13ca434b2c7d60812
Ike Gandhi <ikegandhi@gmail.com>
2014-06-06 14:23:42 -0400

    search string

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,7 +1,8 @@
 var unspoil = function() {
-  var searchTerm = $('#searchTxt').val();
-  alert(searchTerm);
+  $('.tweet-text:contains($('#searchTxt').val()')).each(function(index) {
+    $(this).text('SPOILER!');
+  });
 }
 
-$('#content-main-heading').append(" Keywoprd: <input type='text' id='searchTxt' name='term'></input> <br>");
+$('#content-main-heading').append(" Keyword: <input type='text' id='searchTxt' name='term'></input> <br>");
 $('#content-main-heading').append("<input type='button' value='Unspoil' onclick='unspoil();'></input>");
